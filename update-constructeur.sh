5eb97fbf8b32761bf8595dc40b894b0d2ad1346c
pauljeandel <58475061+pauljeandel@users.noreply.github.com>
2021-12-25 21:43:09 +0100

    up 5

--- update-constructeur.sh
+++ update-constructeur.sh
@@ -1,12 +1,16 @@
 current=$(git branch --show-current)
 if [[ `git status --porcelain` ]]; then
+    echo 'git status FAIL'
     echo "Une erreur est survenue. Impossible de checkout sur la branche" $1 ". Vérifiez que vous n'avez pas de modification en cours."
     exit 1
 else
-    echo 'Verification des mises à jour sur la branche ' $1 '.'
+    echo 'git status OK'
+    echo 'Verification des mises à jour sur la branche' $1' :'
+    echo
     firstcommit=$(git log main..$1 --oneline | tail -1 | cut -c1-8)
-    echo 'Premier commit de la branche ' $1 ' : ' $firstcommit
+    echo 'Premier commit de la branche' $1 ':' $firstcommit
     commitbefore=$(git log main $1 --oneline | tail -1 | cut -c1-8)
+    echo 'Commit précedent :' $commitbefore
     git cherry -v $1 main $commitbefore | grep '^\+' --color
 fi
 
