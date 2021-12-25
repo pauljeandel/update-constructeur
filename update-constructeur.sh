acd3c0d0a0f5b833e4f2e0484c15ccbcbde1b00e
pauljeandel <58475061+pauljeandel@users.noreply.github.com>
2021-12-25 21:45:25 +0100

    aa

--- update-constructeur.sh
+++ update-constructeur.sh
@@ -10,24 +10,9 @@ else
     firstcommit=$(git log main..$1 --oneline | tail -1 | cut -c1-8)
     echo 'Premier commit de la branche' $1 ':' $firstcommit
     commitbefore=$(git log main $1 --oneline | tail -1 | cut -c1-8)
-    echo 'Commit précedent :' $commitbefore
+    echo 'Commit précedent sur la branche principale :' $commitbefore
     git cherry -v $1 main $commitbefore | grep '^\+' --color
 fi
-
-# if [ $? -eq 0 ]; then
-#     echo
-#     #get the first commit ever of the branch $1
-#     firstcommit=$(git log main..$1 --oneline | tail -1 | cut -c1-8)
-#     echo "Le hash du premier commit de la branche est : " $firstcommit
-#     echo
-#     echo "Commits disponibles pour la branche" $1 ":"
-#     git cherry -v $1 main 
-#     echo
-#     git checkout $current
-# else
-#     echo "Une erreur est survenue. Impossible de checkout sur la branche" $1 "."
-#     exit 1
-# fi
 
 # git log --oneline --all | sed -n '/f9712cc/{n;p;}' | cut -c1-8
 # git cherry -v inondation-protection master 9c855e3 | grep '^\+'
