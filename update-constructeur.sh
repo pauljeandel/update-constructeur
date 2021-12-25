1e9a5e98b93093cb6db2df98f758aceec5c78e57
pauljeandel <58475061+pauljeandel@users.noreply.github.com>
2021-12-25 01:05:18 +0100

    lmk

--- update-constructeur.sh
+++ update-constructeur.sh
@@ -1,3 +1,9 @@
-#get the first commit of the branch $1
-git log main..$1 --oneline | tail -1
-echo "Everything is up to date"
+git checkout $1
+echo
+#get the first commit ever of the branch $1
+firstcommit=$(git log main..$1 --oneline | tail -1 | cut -c1-8)
+echo "Le hash du premier commit de la branche est : " $firstcommit
+echo
+echo "Commits disponibles pour la branche" $1 ":"
+git cherry -v $1 main 
+echo
