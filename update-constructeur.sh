820ebf9cd91ec19a06ae39a925db201353a97786
pauljeandel <58475061+pauljeandel@users.noreply.github.com>
2021-12-25 20:01:55 +0100

    up

--- update-constructeur.sh
+++ update-constructeur.sh
@@ -1,9 +1,17 @@
+current=$(git branch --show-current)
 git checkout $1
-echo
-#get the first commit ever of the branch $1
-firstcommit=$(git log main..$1 --oneline | tail -1 | cut -c1-8)
-echo "Le hash du premier commit de la branche est : " $firstcommit
-echo
-echo "Commits disponibles pour la branche" $1 ":"
-git cherry -v $1 main 
-echo
+if [ $? -eq 0 ]; then
+    echo
+    #get the first commit ever of the branch $1
+    firstcommit=$(git log main..$1 --oneline | tail -1 | cut -c1-8)
+    echo "Le hash du premier commit de la branche est : " $firstcommit
+    echo
+    echo "Commits disponibles pour la branche" $1 ":"
+    git cherry -v $1 main 
+    echo
+    git checkout $current
+else
+    echo "Une erreur est survenue. Impossible de checkout sur la branche" $1 "."
+    exit 1
+fi
+
