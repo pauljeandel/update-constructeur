6c305124de2268cf440389e3ea17a47dbe5b05bb
pauljeandel <58475061+pauljeandel@users.noreply.github.com>
2021-12-25 21:58:58 +0100

    fff

--- update-constructeur.sh
+++ update-constructeur.sh
@@ -1,18 +1,45 @@
-current=$(git branch --show-current)
-if [[ `git status --porcelain` ]]; then
-    echo 'git status FAIL'
-    echo "Une erreur est survenue. Impossible de checkout sur la branche" $1 ". Vérifiez que vous n'avez pas de modification en cours."
+# Usage : bash update-constructeur.sh [acf-project-path] [branch-to-check]
+
+if [ -z "$1" ]
+  then
+    echo "FATAL : Pas de chemin de projet ACF spécifié"
     exit 1
 else
-    echo 'git status OK'
-    echo 'Verification des mises à jour sur la branche' $1' :'
-    echo
-    firstcommit=$(git log main..$1 --oneline | tail -1 | cut -c1-8)
-    echo 'Premier commit de la branche' $1 ':' $firstcommit
-    commitbefore=$(git log main $1 --oneline | tail -1 | cut -c1-8)
-    echo 'Commit précedent sur la branche principale :' $commitbefore
-    git cherry -v $1 main $commitbefore | grep '^\+' --color
+    cd $1
+    if [ $? -ne 0 ]
+    then
+        echo "FATAL : Chemin de projet ACF invalide"
+        exit 1
+    fi
+    if [ -z "$2" ]
+    then
+        echo "FATAL : Pas de branche spécifiée"
+        exit 1
+    else
+        if [[ `git status --porcelain` ]]; then
+            echo 'git status FAIL'
+            echo "Une erreur est survenue. Vérifiez que vous n'avez pas de modification en cours."
+            exit 1
+        else
+            echo 'git status OK'
+            if [[ `git branch | grep $2` ]]; then
+                echo 'Verification des mises à jour sur la branche' $2' :'
+                echo
+                firstcommit=$(git log main..$2 --oneline | tail -1 | cut -c1-8)
+                echo 'Premier commit de la branche' $2 ':' $firstcommit
+                commitbefore=$(git log main $2 --oneline | tail -1 | cut -c1-8)
+                echo 'Commit précedent sur la branche principale :' $commitbefore
+                git cherry -v $2 main $commitbefore | grep '^\+' --color
+            else
+                echo 'FATAL : Branche non trouvée :' $2
+                exit 1
+            fi
+        fi
+    fi
 fi
+
+
+
 
 # git log --oneline --all | sed -n '/f9712cc/{n;p;}' | cut -c1-8
 # git cherry -v inondation-protection master 9c855e3 | grep '^\+'
