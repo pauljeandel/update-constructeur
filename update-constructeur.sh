3b44a5121d8ca31bff5595a14fbbbcd55bcfa260
pauljeandel <58475061+pauljeandel@users.noreply.github.com>
2021-12-27 00:58:44 +0100

    update and install

--- update-constructeur.sh
+++ update-constructeur.sh
@@ -1,5 +1,22 @@
 # Usage : bash update-constructeur.sh [acf-project-path] [branch-to-check]
 
+
+if [ "$1" = "install" ]; then
+    echo "Installing globally.."
+    sudo cp update-constructeur.sh /usr/local/bin/update-constructeur
+    sudo chmod +x /usr/local/bin/update-constructeur
+    echo "Done."
+  exit 0
+fi
+
+#if $1 is equal to 'update' then update the script form git with git pull
+if [ "$1" = "update" ]; then
+    cd /usr/local/bin/update-constructeur
+    git pull
+    echo "Done."
+fi
+
+
 if [ -z "$1" ]
   then
     echo "FATAL : Pas de chemin de projet ACF spécifié"
@@ -27,7 +44,9 @@ else
                 echo
                 firstcommit=$(git log master..$2 --oneline | tail -1 | cut -c1-7)
                 echo 'Premier commit de la branche' $2 ':' $firstcommit
-                commitbefore=$(git log master $2 --oneline | sed -n '/'$2'/{n;p;}' | cut -c1-7)
+                #git log master $2 --oneline | sed -n '/'$2'/{n;p;}' | cut -c1-7
+                #git log master $2 --oneline | sed -n "/$firstcommit/{n;p;}"
+                commitbefore=$(git log master $2 --oneline | sed -n "/$firstcommit/{n;p;}" | cut -c1-7)
                 echo 'Commit précedent sur la branche principale :' $commitbefore
                 echo
                 echo 'Verification des mises à jour disponibles sur la branche' $2' :'
